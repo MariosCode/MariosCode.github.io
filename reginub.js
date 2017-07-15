0aea47188242fca63692465ecee201bf08b2d07b
MariosCode <MarioCorteguera@gmail.com>
2017-07-14 20:50:17 -0400

    1

--- reginub.js
+++ reginub.js
@@ -1,4 +1,6 @@
-var name = prompt("Please enter your name", "Lord Reginub");
-if(name != null){
-	alert("Success! Your name is " + name);
-}
+console.log("Reginub script loaded!");
+document.body.innerHTML += '<div id="MC_div1" style="position:absolute;width:32px;height:32px;opacity:0.9;z-index:100;background:#000;background-image:url(\'https://marioscode.github.io/LordNub32x32plus.png\')"></div>';
+
+/*
+var script = document.createElement('script');script.type = 'text/javascript';script.src = 'https://marioscode.github.io/reginub.js';document.body.appendChild(script);
+*/
