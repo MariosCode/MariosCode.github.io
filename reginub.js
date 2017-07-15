5206c01360045f1161a57bbe2873313946feaca1
MariosCode <MarioCorteguera@gmail.com>
2017-07-14 23:10:58 -0400

    1

--- reginub.js
+++ reginub.js
@@ -1,21 +1,28 @@
-var socketioCDN = document.createElement('script');
-socketioCDN.type = 'text/javascript';
-socketioCDN.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js';
-document.body.appendChild(socketioCDN);
+var mc_socketioCDN = document.createElement('script');
+mc_socketioCDN.type = 'text/javascript';
+mc_socketioCDN.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js';
+document.body.appendChild(mc_socketioCDN);
 
-var currentUsername = document.body.innerHTML;
-currentUsername = currentUsername.substring(currentUsername.indexOf("<span class=\"username\">")+23,currentUsername.length);
-currentUsername = currentUsername.substring(0,currentUsername.indexOf("</span>"));
+var mc_currentUsername = document.body.innerHTML;
+mc_currentUsername = mc_currentUsername.substring(mc_currentUsername.indexOf("<span class=\"username\">")+23,mc_currentUsername.length);
+mc_currentUsername = mc_currentUsername.substring(0,mc_currentUsername.indexOf("</span>"));
 
-var MC_div1 = document.createElement("div");
-Object.assign(MC_div1.style,{position: "absolute", top: "0px", width: "32px", height: "32px", opacity: "0.9", background: "#000", backgroundImage: "url('https://marioscode.github.io/LordNub32x32Dark.png')", cursor: "pointer", display: "inline"});
-MC_div1.onmouseover = function(){
-	MC_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Light.png')";
+
+//preload images
+var mc_img_LordNub32x32Dark = new Image();
+mc_img_LordNub32x32Dark.src = 'https://marioscode.github.io/LordNub32x32Dark.png';
+var mc_img_LordNub32x32Light = new Image();
+mc_img_LordNub32x32Light.src = 'https://marioscode.github.io/LordNub32x32Light.png';
+
+var mc_div1 = document.createElement("div");
+Object.assign(mc_div1.style,{position: "absolute", bottom: "0px", width: "32px", height: "32px", opacity: "0.9", background: "#000", backgroundImage: "url('https://marioscode.github.io/LordNub32x32Dark.png')", cursor: "pointer", display: "inline"});
+mc_div1.onmouseover = function(){
+	mc_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Light.png')";
 };
-MC_div1.onmouseout = function(){
-	MC_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Dark.png')";
+mc_div1.onmouseout = function(){
+	mc_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Dark.png')";
 };
-document.body.appendChild(MC_div1);
+document.body.appendChild(mc_div1);
 /*
 var script = document.createElement('script');script.type = 'text/javascript';script.src = 'https://marioscode.github.io/reginub.js';document.body.appendChild(script);
 */
