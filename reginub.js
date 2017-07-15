80775d8395024ff717470918a7d5a718f83b405e
MariosCode <MarioCorteguera@gmail.com>
2017-07-14 20:56:26 -0400

    1

--- reginub.js
+++ reginub.js
@@ -1,5 +1,13 @@
 console.log("Reginub script loaded!");
-document.body.innerHTML += '<div id="MC_div1" style="position:absolute;width:32px;height:32px;opacity:0.9;z-index:100;background:#000;background-image:url(\'https://marioscode.github.io/LordNub32x32plus.png\')"></div>';
+var MC_div1 = document.createElement('div');
+MC_div1.style.position = "absolute";
+MC_div1.style.width = "32px";
+MC_div1.style.height = "32px";
+MC_div1.style.opacity = "0.9";
+MC_div1.style.background = "#000";
+MC_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32plus.png')";
+document.body.appendChild(MC_div1);
+//document.body.innerHTML += '<div id="MC_div1" style="position:absolute;width:32px;height:32px;opacity:0.9;z-index:100;background:#000;background-image:url(\'https://marioscode.github.io/LordNub32x32plus.png\')"></div>';
 
 /*
 var script = document.createElement('script');script.type = 'text/javascript';script.src = 'https://marioscode.github.io/reginub.js';document.body.appendChild(script);
