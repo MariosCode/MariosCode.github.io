6fccb5749e6153443ad95d2864d9165072f123be
MariosCode <MarioCorteguera@gmail.com>
2017-07-15 16:36:24 -0400

    1

--- reginub.js
+++ reginub.js
@@ -14,14 +14,37 @@ mc_img_LordNub32x32Dark.src = 'https://marioscode.github.io/LordNub32x32Dark.png
 var mc_img_LordNub32x32Light = new Image();
 mc_img_LordNub32x32Light.src = 'https://marioscode.github.io/LordNub32x32Light.png';
 
+//reginub/discord mode button
 var mc_div1 = document.createElement("div");
-Object.assign(mc_div1.style,{position: "absolute", bottom: "0px", width: "32px", height: "32px", opacity: "0.9", background: "#000", backgroundImage: "url('https://marioscode.github.io/LordNub32x32Dark.png')", cursor: "pointer", display: "inline"});
+Object.assign(mc_div1.style,{position: "absolute", bottom: "0px", width: "32px", height: "32px", opacity: "0.9", background: "#000", backgroundImage: "url('https://marioscode.github.io/LordNub32x32Dark.png')", cursor: "pointer", display: "inline", zIndex: "1000000"});
 mc_div1.onmouseover = function(){
 	mc_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Light.png')";
 };
 mc_div1.onmouseout = function(){
 	mc_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32Dark.png')";
 };
+mc_div1.onclick = function(){
+	if(mc_div2.style.display == "none"){
+		mc_div2.style.display = "block";
+	}else{
+		mc_div2.style.display = "none";
+	}
+};
+
+//9001 - main div, container of everything
+var mc_div2 = document.createElement("div");
+Object.assign(mc_div2.style,{position: "absolute", top: "0px", width: "100%", height: "100%", opacity: "1", background: "#0FF", display:"none", zIndex: "9001"});
+document.body.appendChild(mc_div2);
+
+//9002 - iframe for petals around the rose game
+var mc_iframe_game_petalsaroundtherose = document.createElement("iframe");
+        mc_iframe_game_petalsaroundtherose.setAttribute("src", "https://MariosCode.github.io/petalsAroundTheRose.html");
+        Object.assign(mc_iframe_game_petalsaroundtherose.style,{position: "absolute", top: "0px", width: "100%", height: "100%", display:"none", zIndex: "9002"});
+        mc_div2.appendChild(mc_iframe_game_petalsaroundtherose);
+
+
+
+//infinite - reginub/discord mode button, it should be on top of everything else
 document.body.appendChild(mc_div1);
 /*
 var script = document.createElement('script');script.type = 'text/javascript';script.src = 'https://marioscode.github.io/reginub.js';document.body.appendChild(script);
