44e708aca445ab0c3d5be838dd897a8ec7c9c0b1
MariosCode <MarioCorteguera@gmail.com>
2017-07-14 20:59:21 -0400

    1

--- reginub.js
+++ reginub.js
@@ -6,7 +6,7 @@ MC_div1.style.height = "32px";
 MC_div1.style.opacity = "0.9";
 MC_div1.style.background = "#000";
 MC_div1.style.backgroundImage = "url('https://marioscode.github.io/LordNub32x32plus.png')";
-document.body.appendChild(MC_div1);
+document.getElementById("app-mount").appendChild(MC_div1);
 //document.body.innerHTML += '<div id="MC_div1" style="position:absolute;width:32px;height:32px;opacity:0.9;z-index:100;background:#000;background-image:url(\'https://marioscode.github.io/LordNub32x32plus.png\')"></div>';
 
 /*
